79dc19b0ce24df1c223a775fe200d808c851cd5b
Stanislav Balia <balya.stanislav@gmail.com>
2020-10-30 23:53:21 +0200

    init

---
+++ SampleFastMember/Program.cs
@@ -0,0 +1,25 @@
+﻿using FastMember;
+
+namespace SampleFastMember
+{
+    enum SomeEnum
+    {
+        Hi = 1,
+        Bye = 2
+    }
+    
+    class Simple
+    {
+        public SomeEnum? Value { get; set; }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var instance = new Simple();
+            var accessor = TypeAccessor.Create(typeof(Simple));
+            accessor[instance, "Value"] = 2;
+        }
+    }
+}
